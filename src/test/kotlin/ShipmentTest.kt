876cbd840b985894fbbb4b8dfe451686dfa3db3b
Nate Thomson <n8thomson@outlook.com>
2022-05-25 23:52:39 -0600

    Final

--- src/test/kotlin/ShipmentTest.kt
+++ src/test/kotlin/ShipmentTest.kt
@@ -13,5 +13,13 @@ internal class ShipmentTest {
         assertTrue(s.updateHistory[s.updateHistory.size-1] == "ballin")
     }
 
+    @Test
+    fun addNote() {
+        var s: Shipment = Shipment("hungry", "s10000")
+        s.addUpdate("ballin")
+        assertTrue(s.updateHistory[s.updateHistory.size-1] == "ballin")
+    }
+
+
 
 }
