b056981f8524b165754250e8ca4212750b34f7e4
Nate T <n8thomson@outlook.com>
2022-06-05 22:45:10 -0600

    webpage is serving

--- src/main/kotlin/Serve.kt
+++ src/main/kotlin/Serve.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.engine.*
+import io.ktor.server.http.content.*
 import java.io.File
 
 fun main(args: Array<String>) {
@@ -13,7 +14,7 @@ fun main(args: Array<String>) {
                 val file = File("webpage.html")
                 val contents = file.readText()
                 call.respondText(contents, ContentType.Text.Html)
-
+              call.respondText("Hello, world!", ContentType.Text.Html)
             }
         }
     }.start(wait = true)
